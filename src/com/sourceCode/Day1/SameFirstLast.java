7e421852a851f250a5cde29d4f7aefc08afdbf69
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-16 23:58:04 +0530

    initial commit

--- src/com/sourceCode/Day1/SameFirstLast.java
+++ src/com/sourceCode/Day1/SameFirstLast.java
@@ -2,7 +2,7 @@ package com.sourceCode.Day1;
 
 public class SameFirstLast {
     public static void main(String[] args) {
-
+//Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
 
     }
 
