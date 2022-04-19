1bf5a2ecae1ae60921bc9d10bf57cefeb3db3a92
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-19 20:31:25 +0530

    initial commit

---
+++ src/com/sourceCode/Day6/Application.java
@@ -0,0 +1,21 @@
+package com.sourceCode.Day6;
+
+import java.util.HashSet;
+
+public class Application {
+    public static void main(String[] args) {
+        HashSet<Integer> values = new HashSet<Integer>(); //duplicate values are not saved in hashset & it doesnt care about the order of output
+        values.add(12);                                   //hence only UNIQUE values;
+                                                          //linkedHashSet stored and output in ORDER but no duplicates allowed
+        values.add(23);
+        values.add(546);
+        values.add(23);
+        values.add(33);
+
+        for (Integer value: values
+             ) {
+            System.out.println(value);
+
+        }
+    }
+}
