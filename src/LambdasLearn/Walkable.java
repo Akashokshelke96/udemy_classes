c7fea280104f28614c1058a9fb43065f9c763285
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-05-04 23:52:47 +0530

    lambdas Learning

---
+++ src/LambdasLearn/Walkable.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Walkable {
+    public void walk();//it wont take any other method as its a functional interface
+}
---
+++ src/LambdasLearn/revision/Walkable.java
@@ -0,0 +1,5 @@
+package revision;
+
+public interface Walkable {
+    public void walk();
+}
